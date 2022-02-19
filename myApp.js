7f587418d6d93f71b14b33b0057fb904adb37096
Jemi-code <81371274+Jemi-code@users.noreply.github.com>
2022-02-19 10:34:56 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res) {
 
   app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+  res.json({message: "Hello json"});
+});
+
 
 
 
